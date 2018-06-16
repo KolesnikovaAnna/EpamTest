2fac9256dedb7eca2110906f8fe0d84b83a7e74c
KolesnikovaAnna <31996544+KolesnikovaAnna@users.noreply.github.com>
2018-06-16 17:54:59 +0300

    Delete DrivPath.java

--- src/main/java/com/spbstu/lab5/DrivPath.java
+++
@@ -1,7 +0,0 @@
-package com.spbstu.lab5;
-
-public enum DrivPath {
-    DRIVER_PATH("C:\\Users\\X555La\\Downloads\\chromedriver_win32\\");
-    public String Str;
-    DrivPath(String str) { Str = str;}
-}
